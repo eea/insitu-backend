a60fab450f49ec8b0a0b3c84b8a762e1e0ef2c5e
valentinab25 <valentinab25>
2024-01-30 20:49:47 +0200

    test: fix test timeout

--- test/tests/plone-relstorage/run.sh
+++ test/tests/plone-relstorage/run.sh
@@ -13,6 +13,8 @@ zname="relstorage-container-$RANDOM-$RANDOM"
 zpull="$(docker pull postgres:9-alpine)"
 zid="$(docker run -d --name "$zname" -e POSTGRES_USER=plone -e POSTGRES_PASSWORD=plone -e POSTGRES_DB=plone postgres:9-alpine)"
 
+sleep 20
+
 # Start Plone as RelStorage Client
 pname="plone-container-$RANDOM-$RANDOM"
 pid="$(docker run -d --name "$pname" --link=$zname:db -e RELSTORAGE_DSN="dbname='plone' user='plone' host='db' password='plone'" "$image")"
